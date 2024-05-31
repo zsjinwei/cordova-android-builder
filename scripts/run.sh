f01c22f5cd04d39d388eb8ddb510e9bbb7ced78a
huangjinwei <zsjinwei@foxmail.com>
2024-05-31 11:02:00 +0800

    add git pull

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git pull
+
 docker run -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro  -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:ro -v ./scripts:/opt/scripts -v ./dist:/opt/dist  --rm zsjinwei/cordova-android-builder:1.0.1 bash
 
