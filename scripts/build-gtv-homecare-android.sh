306b6f498512b635f397571657a48844407e2166
huangjinwei <zsjinwei@foxmail.com>
2024-05-31 17:36:41 +0800

    update build script

--- scripts/build-gtv-homecare-android.sh
+++ scripts/build-gtv-homecare-android.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VENDOR=gtv
+
 cd /opt/src
 (sleep 5 && while [ 1 ]; do sleep 5; echo yes; done) | git clone git@git.tigeek.com:huangjinwei/home-care-service-phone.git
 cd /opt/src/home-care-service-phone
@@ -7,10 +9,11 @@ export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=file:///tmp/gradle-7.6-all.zip
 yarn install
 cd src-cordova
 mkdir www
+rm ./package.json && cp ../vendor/${VENDOR}/cordova/package.json ./package.json
 cordova platform add android
 echo "systemProp.jdk.tls.client.protocols=TLSv1.2,TLSv1.3" >> platforms/android/gradle.properties
 echo "systemProp.https.protocols=TLSv1.2,TLSv1.3" >> platforms/android/gradle.properties
 cd ..
 # npx quasar build -m android
-QUASAR_CLI="npx quasar" ./release.sh android gtv yes
+QUASAR_CLI="npx quasar" ./release.sh android ${VENDOR} yes
 cp ./dist/cordova/android/apk/release/* /opt/dist/
--- scripts/build-yishou-homecare-android.sh
+++ scripts/build-yishou-homecare-android.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VENDOR=yishou
+
 cd /opt/src
 (sleep 5 && while [ 1 ]; do sleep 5; echo yes; done) | git clone git@git.tigeek.com:huangjinwei/home-care-service-phone.git
 cd /opt/src/home-care-service-phone
@@ -7,11 +9,12 @@ export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=file:///tmp/gradle-7.6-all.zip
 yarn install
 cd src-cordova
 mkdir www
+rm ./package.json && cp ../vendor/${VENDOR}/cordova/package.json ./package.json
 cordova platform add android
 echo "systemProp.jdk.tls.client.protocols=TLSv1.2,TLSv1.3" >> platforms/android/gradle.properties
 echo "systemProp.https.protocols=TLSv1.2,TLSv1.3" >> platforms/android/gradle.properties
 cd ..
 yarn add gifsicle
 # npx quasar build -m android
-QUASAR_CLI="npx quasar" ./release.sh android yishou yes
+QUASAR_CLI="npx quasar" ./release.sh android ${VENDOR} yes
 cp ./dist/cordova/android/apk/release/* /opt/dist/
