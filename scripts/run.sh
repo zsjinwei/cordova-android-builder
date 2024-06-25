3399ae0bb9069847e562aaeb258d00cf4043f04a
huangjinwei <zsjinwei@foxmail.com>
2024-06-25 14:40:37 +0800

    update run scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 
 git pull
 
-docker run -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro  -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:ro -v ./scripts:/opt/scripts -v ./dist:/opt/dist -v ./prebuilds:/opt/prebuilds  --rm zsjinwei/cordova-android-builder:latest bash
+docker run -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro  -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:ro -v ./scripts:/opt/scripts -v ./dist:/opt/dist -v ./prebuilds:/opt/prebuilds  --rm registry.cn-hangzhou.aliyuncs.com/zsjinwei/cordova-android-builder:latest bash
