24411408ca15e1608655515a57d928e377a1a92c
huangjinwei <zsjinwei@foxmail.com>
2024-06-25 11:29:32 +0800

    update build script

--- scripts/build-yishou-homecare-android.sh
+++ scripts/build-yishou-homecare-android.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 export VENDOR=yishou
+export PROJECT_NAME=home-care-service-phone
+export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=file:///tmp/gradle-7.6-all.zip
+
+mkdir /opt/src
+mkdir /opt/dist
+
+if [ -d "/opt/src/${PROJECT_NAME}" ]; then
+  echo "project directory already exists"
+else
+  echo "project directory is not exists"
+  cd /opt/src
+  (sleep 5 && while [ 1 ]; do sleep 5; echo yes; done) | git clone git@git.tigeek.com:huangjinwei/home-care-service-phone.git
+fi
 
 cp -ri /opt/src /opt/src-${VENDOR}
-cd /opt/src-${VENDOR}
-(sleep 5 && while [ 1 ]; do sleep 5; echo yes; done) | git clone git@git.tigeek.com:huangjinwei/home-care-service-phone.git
-cd /opt/src-${VENDOR}/home-care-service-phone
-export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=file:///tmp/gradle-7.6-all.zip
+cd /opt/src-${VENDOR}/${PROJECT_NAME}
+
 yarn install
 cd src-cordova
 mkdir www
@@ -19,4 +30,4 @@ yarn add gifsicle
 # npx quasar build -m android
 QUASAR_CLI="npx quasar" ./release.sh android ${VENDOR} yes
 # cp ./dist/cordova/android/apk/release/* /opt/dist/
-cp /opt/src-${VENDOR}/home-care-service-phone/dist/cordova/android/apk/release/yishou-com.yishou.homecare.apk /opt/dist/
+cp /opt/src-${VENDOR}/${PROJECT_NAME}/dist/cordova/android/apk/release/yishou-com.yishou.homecare.apk /opt/dist/
